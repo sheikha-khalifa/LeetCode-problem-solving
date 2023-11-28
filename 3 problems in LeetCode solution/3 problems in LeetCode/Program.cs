73d6bf5e7deeb5e0850985476b27da11f5c09c8e
sheikha ALQuyudhi <alqyoodhi181@gmail.com>
2023-11-27 22:53:25 -0800

    new updates

---
+++ 3 problems in LeetCode solution/3 problems in LeetCode/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _3_problems_in_LeetCode
+{
+    internal class Program
+    {
+        static int IndexOfNeedleInHaystack(string needle, string haystack)
+        {
+            int index = haystack.IndexOf(needle);
+            return index;
+        }
+        static void Main(string[] args)
+        {
+            string needle = " ";
+            string heystack = " ";
+            Console.WriteLine("please writer heystack:");
+            heystack = Console.ReadLine();
+            Console.WriteLine("please writer needle:");
+            needle = Console.ReadLine();
+            int result = IndexOfNeedleInHaystack(needle, heystack);
+
+            if (result >= 0)
+            {
+                Console.WriteLine($"The needle was found at index: {result}");
+            }
+            else
+            {
+                Console.WriteLine("The needle was not found in the haystack.");
+            }
+        }
+    }
+}
